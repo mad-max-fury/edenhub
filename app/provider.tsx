ee96ecabe10eb81ed4d8dab7fd945dc2349dec2a
mad-max-fury <chrisnonso8@gmail.com>
2026-06-27 19:33:56 +0100

    --feat: fixed

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,20 +1,25 @@
 "use client";
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { APIProvider } from "@vis.gl/react-google-maps";
 import { ReduxProvider } from "@/redux";
 import NotificationContainer from "@/components/notifications/notificationContainer";
 import { CartSync } from "@/components/cart/CartSync";
 import { AuthModalProvider } from "@/components/authModal/AuthModal";
 import { googleClientId } from "@/config";
 
+const GOOGLE_MAPS_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY || "";
+
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <GoogleOAuthProvider clientId={googleClientId}>
-      <ReduxProvider>
-        <NotificationContainer />
-        <CartSync />
-        <AuthModalProvider>{children}</AuthModalProvider>
-      </ReduxProvider>
+      <APIProvider apiKey={GOOGLE_MAPS_KEY}>
+        <ReduxProvider>
+          <NotificationContainer />
+          <CartSync />
+          <AuthModalProvider>{children}</AuthModalProvider>
+        </ReduxProvider>
+      </APIProvider>
     </GoogleOAuthProvider>
   );
 };
